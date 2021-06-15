20d962ec8f58aeca6f5359e2b636045e1682b4ab
vick.zhu <vick.zhu@rebatesme.com>
2021-06-15 19:35:51 +0800

    優化

---
+++ pentagon-biz/src/main/java/com/pentagon/biz/dto/RoleUri.java
@@ -0,0 +1,24 @@
+package com.pentagon.biz.dto;
+
+public class RoleUri {
+
+	private Long roleId;
+	private String uri;
+
+	public Long getRoleId() {
+		return roleId;
+	}
+
+	public void setRoleId(Long roleId) {
+		this.roleId = roleId;
+	}
+
+	public String getUri() {
+		return uri;
+	}
+
+	public void setUri(String uri) {
+		this.uri = uri;
+	}
+
+}
